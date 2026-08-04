2945fa4fd1689d750e76c88984657cc498fcbc93
vags2105-tec <vags2105@gmail.com>
2026-08-04 22:57:04 +0530

    OOP's concept today

---
+++ OOPS/practice2.py
@@ -0,0 +1,12 @@
+class Laptop:
+    brand="HP"
+    RAM="8GB"
+    price="50000"
+ob1=Laptop()
+ob1.brand="Macbook"
+ob1.RAM="16GB"
+print("Brand name is:",ob1.brand)
+print("RAM is:",ob1.RAM)
+ob2=Laptop()
+ob2.brand="Lenova"
+print("Brand name is:",ob2.brand)
