b58964131c7f2fe8c0b75d08eaa3c6227f286a06
vags2105-tec <vags2105@gmail.com>
2026-08-04 22:17:00 +0530

    Added OOPs folder and class.py file

--- OOPS/class.py
+++ OOPS/class.py
@@ -2,6 +2,8 @@ class Vehicle:
     color="red"
     petrolordiesel="petrol"
     mileage="10"
-c1=Vehicle()
-print(c1.color)
-print(c1.mileage)
+car=Vehicle()
+print(car.color)
+print(car.mileage)
+bike=Vehicle()
+print(bike.petrolordiesel)
