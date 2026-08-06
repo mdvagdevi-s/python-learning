fb69825ec2ee4f1badd4c80f047f1d45be8757f4
vags2105-tec <mdvagdevi@gmail.com>
2026-08-06 19:34:10 +0530

    Added practice3

--- OOPS/practice3.py
+++ OOPS/practice3.py
@@ -1,14 +1,14 @@
 # create class student that takes 3 marks and has method average().
 
 class Student:
-    def __init__(self,m1,m2,m3):
-        self.m1=m1
-        self.m2=m2
-        self.m3=m3
-
+    def __init__(self,name,list_of_marks):
+        self.name=name
+        self.list_of_marks=list_of_marks
     def average(self):
-        avg=(self.m1+self.m2+self.m3)/3
-        return avg
-s1=Student(10,20,30)
-
-print("AVERAGE:",s1.average())
+        sum=0
+        for eachvalue in self.list_of_marks:
+            sum=sum+eachvalue
+        average=sum/3
+        print("Average is:",average)
+student1=Student("Adi",[90,98,99])
+student1.average()
