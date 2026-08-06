a64058775d31169abd7517f5abfb5feeddacd977
vags2105-tec <mdvagdevi@gmail.com>
2026-08-06 20:50:26 +0530

    Changes in encpasulation

--- OOPS/encapsulation.py
+++ OOPS/encapsulation.py
@@ -1,10 +1,10 @@
 class BankAccount:
     def __init__(self,balance):
-        self.balance=balance
+        self._balance=balance
     def deposit(self,amount):
-        self.balance+=amount
+        self._balance+=amount
     def show_balance(self):
-        print("Balance is:",self.balance)
+        print("Balance is:",self._balance)
 acc=BankAccount(1000)
 acc.deposit(10000)
 acc.show_balance()
