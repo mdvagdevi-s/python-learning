7767393437272905cb8525bc0389c6ac5fe7d8c2
vags2105-tec <mdvagdevi@gmail.com>
2026-08-07 21:10:04 +0530

    Practiced inheritance topic

---
+++ OOPS/practice11.py
@@ -0,0 +1,13 @@
+class person:
+    def name(self):
+        print("My name is Vagdevi")
+class student(person):
+    def name(self):
+        print("My name is Vagdvei")
+    def roll_no(self):
+        print("My roll no: 4kv24cs159")
+
+
+p=student()
+p.name()
+p.roll_no()
