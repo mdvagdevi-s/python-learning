e9e61d64144b85f71625243c9534dd9cb65f728e
vags2105-tec <mdvagdevi@gmail.com>
2026-08-07 20:30:54 +0530

    Added practice7

---
+++ OOPS/practice7.py
@@ -0,0 +1,9 @@
+class student:
+    name="Vagdevi"
+    age="20"
+    grade="A"
+
+display=student()
+print("Student name is:",display.name)
+print("Her age is:",display.age)
+print("Her grade is:",display.grade)
