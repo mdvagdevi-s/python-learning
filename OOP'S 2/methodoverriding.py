b27a621748f81438a6615ed1f6ba4bf9eaa62535
vags2105-tec <mdvagdevi@gmail.com>
2026-08-08 20:50:15 +0530

    Program for Methodoverridding

---
+++ OOP'S 2/methodoverriding.py
@@ -0,0 +1,10 @@
+class person:
+    def introduce(self):
+        print("I'm Vagdevi")
+
+class student(person):
+    def introduce(self):
+        print("Im Vagdevi and im student")
+
+s=student()
+s.introduce()
