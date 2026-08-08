5350673daf31bf05e0f6080d8f64810cbefc01a3
vags2105-tec <mdvagdevi@gmail.com>
2026-08-08 16:42:48 +0530

    Updated practice10

--- OOPS/pratice10.py
+++ OOPS/pratice10.py
@@ -1,15 +1,15 @@
 class bank:
     def __init__(self,balance):
-        self.balance=balance
+        self.__balance=balance
     def deposit(self,amount):
-        self.balance+=amount
+        self.__balance+=amount
     def withdraw(self,amount):
-        self.balance-=amount
+        self.__balance-=amount
 
     def show_balance(self):
-        return self.balance
+        return self.__balance
 
 acc=bank(1000)
-acc.deposit(2000)
+acc.deposit(200)
 acc.withdraw(500)
 print(acc.show_balance())
