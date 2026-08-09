5176b2549db4b05fd1773c78aa6b5c497f837e9c
vags2105-tec <mdvagdevi@gmail.com>
2026-08-09 19:17:00 +0530

    Multiple inhertance

---
+++ OOP'S 2/multipleinheritance.py
@@ -0,0 +1,16 @@
+class father:
+    def skill1(self):
+        print("Father knows farming")
+
+class mother:
+    def skill2(Self):
+        print("Mothers knows cooking well")
+
+class child(father,mother):
+    def skill3(self):
+        print("Child knows coding")
+
+c=child()
+c.skill1()
+c.skill2()
+c.skill3()
