4f5daf8677e4212d91c4082e7876cfa6e5bcb1f1
vags2105-tec <mdvagdevi@gmail.com>
2026-08-11 16:43:57 +0530

    Student user-defined module

---
+++ Advance python/main2.py
@@ -0,0 +1,10 @@
+from student import Student
+
+s= Student("Vagdevi",85)
+s.display()
+
+
+#import student
+#Then you need:
+
+#s = student.Student("Vagdevi", 85)
---
+++ Advance python/student.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+    def display(self):
+        print("Name:",self.name)
+        print("Marks:",self.marks)
+        
