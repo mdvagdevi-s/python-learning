a8c8d6ad8a28fed453504a3bb8945f7decdb8477
vags2105-tec <mdvagdevi@gmail.com>
2026-08-28 16:32:07 +0530

    Regular expression concepts

--- Advance python/regex.py
+++ Advance python/regex.py
@@ -1,5 +1,43 @@
 import re
 text="apple banana mango orange"
-
 result=re.findall(r"[ab]",text)
 print(result)
+
+a="Python 123 AI 456"
+res=re.findall(r"[0-9]+",a)
+print(res)
+
+text = "Python 123 AI"
+result = re.findall(r"\w+", text)
+print(result)
+
+b = "My PIN is 1234"
+result = re.findall(r"\d{4}", b)
+print(result)
+
+
+c="I love Java"
+r=re.sub(r"Java","Python",c)
+print(r)
+
+
+d = "I have 3 cats and 2 dogs"
+s= re.sub(r"\d+", "many", d)
+print(s)
+
+
+e = "Hello Vagdevi"
+t = re.search(r"^Hello", e)
+print(t.group())
+#^ → beginning
+#$ → end
+
+f = "My marks are 85"
+u= re.search(r"(\d)(\d)", f)
+print(u.group())
+print(u.group(1))
+print(u.group(2))
+
+g="I Like Python"
+v=re.search(r"Python|Java",g)
+print(v.group())
