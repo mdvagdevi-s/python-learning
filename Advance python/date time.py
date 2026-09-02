71d1f76c2ab57a7503f8739a9acc5b6e9a7cdbc1
vags2105-tec <mdvagdevi@gmail.com>
2026-09-02 16:14:17 +0530

    DATETIME CONCEPT

---
+++ Advance python/date time.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+now=datetime.now()
+print(now)
+
+
+#datetime.now() → current date & time
+#.year, .month, .day → access parts
+#strftime() → datetime → string
+#strptime() → string → datetime
+#timedelta() → add/subtract time
