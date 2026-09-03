c057752ac96a43d6ed0fa9280f50a9c8728c9eed
vags2105-tec <mdvagdevi@gmail.com>
2026-09-03 19:18:17 +0530

    Typehints concpet

---
+++ Advance python/type_hints .py
@@ -0,0 +1,12 @@
+def add (a:int,b:int)->int:
+    return a+b
+
+def greet(name: str) -> str:
+    return "Hello " + name
+
+def total(num:list[int])->int:
+    return sum(num)
+
+
+def marks(data: dict[str, int]) -> None:
+    print(data)
